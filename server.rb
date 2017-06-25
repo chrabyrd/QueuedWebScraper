fc7347c18c024ef2d5a45c59396224e54fae568f
Christopher <chrabyrd@gmail.com>
2017-06-25 09:28:23 -0700

    Add WebData model, index and show routes

--- server.rb
+++ server.rb
@@ -15,7 +15,33 @@ Thread.new do
   end
 end
 
+# Model
+
+class WebData
+  include Mongoid::Document
+
+  field :url, type: String
+  field :html, type: String, default: "202 pending"
+  validates :url, presence: true
+end
+
 # Routes (Apparently Sinatra doesn't do controllers)
+
+namespace '/api' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/index' do # Index
+    WebData.all.to_json
+  end
+
+  get '/:id' do |id| # Show
+    data_object = WebData.where(id: id).first
+    halt(404, { message:'Object Not Found'}.to_json) unless data_object
+    data_object.to_json
+  end
+end
 
 get '/' do
   val += 1
