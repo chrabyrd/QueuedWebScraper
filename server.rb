5113d58cdc29ea7dd9d45420c941451c0270c141
Christopher <chrabyrd@gmail.com>
2017-06-25 08:23:38 -0700

    Hello World

--- server.rb
+++ server.rb
@@ -1,3 +1,11 @@
 require 'sinatra'
 require 'sinatra/namespace'
 require 'mongoid'
+
+Mongoid.load! 'mongoid.config'
+
+# Routes (Apparently Sinatra doesn't do controllers)
+
+get '/' do
+  "Hello World!"
+end
